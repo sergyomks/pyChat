98fd419071389f313cb735f34170a35c6226f575
sergyomks <sergyomks7@gmail.com>
2025-10-24 15:06:36 -0500

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -53,4 +53,5 @@ def get_messages():
     return jsonify({'messages': messages})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
+
